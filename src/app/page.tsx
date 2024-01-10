65c69e56e5a09603d1fd282ad44689d413f10c9f
Osita <chrisjerry070@gmail.com>
2024-01-10 23:58:09 +0400

    working on nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="">
-      
+      <section className=' uppercase'>
+        <h1>hi, i am osita jerry</h1>
+        <p>A front-end developer, who is passionate about building accessible and user friendly websites.</p>
+        <button>contact me <span></span></button>
+      </section>
+      <section></section>
     </main>
   )
 }
