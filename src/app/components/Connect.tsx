48d6e3d879119bcf108c89d7b79a84979940df2f
Osita <chrisjerry070@gmail.com>
2024-01-20 12:10:12 +0400

    created adjusted code

--- src/app/components/Connect.tsx
+++ src/app/components/Connect.tsx
@@ -15,16 +15,8 @@ export const Connect = () => {
         let's connect
       </h1>
 
-      <div className=" flex flex-row-reverse justify-between items-center px-14 mt-20 ">
-        <div className=" w-[40%] text-white flex flex-col-reverse ">
-          <p
-            className={`w-[500px]  py-4 pl-20 bg-gradient-to-l from-[#8F00FF] to-black     text-lg tracking-wide   md:leading-6  md:text-xl text-[#c7c7c7]  ${SpaceGrotesk.className}`}
-          >
-            I am open to remote and onsite full-&nbsp;time, part-time, and contract
-            frontend web development jobs.
-          </p>
-         
-
+      <div className=" flex flex-row-reverse justify-between items-start px-14 mt-20 ">
+        <div className=" w-[40%] text-white flex  ">
           <div className=" ">
             <p
               className={`   font-semibold  uppercase text-[#000] text-xl   md:text-lg    dark:text-white ${SpaceGrotesk.className}`}
@@ -74,26 +66,41 @@ export const Connect = () => {
                 </div>
 
                 <div className=" icon bg-[#222] h-[54px] w-[54px] cursor-pointer  rounded-full flex justify-center items-center ">
-                <a href="https://github.com/ossydimma">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="26"
-                    height="26"
-                    viewBox="0 0 26 26"
-                    fill="none"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M13.0282 2.16675C7.06008 2.16675 2.223 7.00383 2.223 12.9719C2.223 17.7451 5.31808 21.7957 9.61242 23.2257C10.153 23.3232 10.348 22.9906 10.348 22.7046C10.348 22.4478 10.3393 21.7675 10.3361 20.8673C7.32983 21.5194 6.695 19.4178 6.695 19.4178C6.20533 18.1698 5.49575 17.8372 5.49575 17.8372C4.51533 17.1666 5.5705 17.1818 5.5705 17.1818C6.656 17.2576 7.22475 18.2954 7.22475 18.2954C8.18892 19.9464 9.75542 19.4698 10.3686 19.1935C10.4672 18.4948 10.7488 18.0181 11.0565 17.7483C8.658 17.4764 6.136 16.5491 6.136 12.4075C6.136 11.2299 6.55742 10.2636 7.24533 9.50958C7.13592 9.2355 6.76217 8.13592 7.35258 6.64958C7.35258 6.64958 8.25933 6.35817 10.3231 7.75567C11.2045 7.51589 12.1137 7.3935 13.0271 7.39167C13.9405 7.39315 14.8498 7.51554 15.7311 7.75567C17.7959 6.35708 18.7016 6.64958 18.7016 6.64958C19.292 8.13592 18.9215 9.2355 18.8088 9.50958C19.5022 10.2636 19.9182 11.2288 19.9182 12.4075C19.9182 16.5599 17.394 17.4721 14.9868 17.7397C15.3714 18.0733 15.7181 18.732 15.7181 19.7395C15.7181 21.1847 15.7051 22.3503 15.7051 22.7046C15.7051 22.9938 15.8979 23.3297 16.4493 23.2235C20.7415 21.7913 23.8333 17.744 23.8333 12.9719C23.8333 7.00383 18.9963 2.16675 13.0282 2.16675Z"
-                      fill="#00FDEE"
-                    />
-                  </svg>
-                </a>
-              </div>
+                  <a href="https://github.com/ossydimma">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="26"
+                      height="26"
+                      viewBox="0 0 26 26"
+                      fill="none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M13.0282 2.16675C7.06008 2.16675 2.223 7.00383 2.223 12.9719C2.223 17.7451 5.31808 21.7957 9.61242 23.2257C10.153 23.3232 10.348 22.9906 10.348 22.7046C10.348 22.4478 10.3393 21.7675 10.3361 20.8673C7.32983 21.5194 6.695 19.4178 6.695 19.4178C6.20533 18.1698 5.49575 17.8372 5.49575 17.8372C4.51533 17.1666 5.5705 17.1818 5.5705 17.1818C6.656 17.2576 7.22475 18.2954 7.22475 18.2954C8.18892 19.9464 9.75542 19.4698 10.3686 19.1935C10.4672 18.4948 10.7488 18.0181 11.0565 17.7483C8.658 17.4764 6.136 16.5491 6.136 12.4075C6.136 11.2299 6.55742 10.2636 7.24533 9.50958C7.13592 9.2355 6.76217 8.13592 7.35258 6.64958C7.35258 6.64958 8.25933 6.35817 10.3231 7.75567C11.2045 7.51589 12.1137 7.3935 13.0271 7.39167C13.9405 7.39315 14.8498 7.51554 15.7311 7.75567C17.7959 6.35708 18.7016 6.64958 18.7016 6.64958C19.292 8.13592 18.9215 9.2355 18.8088 9.50958C19.5022 10.2636 19.9182 11.2288 19.9182 12.4075C19.9182 16.5599 17.394 17.4721 14.9868 17.7397C15.3714 18.0733 15.7181 18.732 15.7181 19.7395C15.7181 21.1847 15.7051 22.3503 15.7051 22.7046C15.7051 22.9938 15.8979 23.3297 16.4493 23.2235C20.7415 21.7913 23.8333 17.744 23.8333 12.9719C23.8333 7.00383 18.9963 2.16675 13.0282 2.16675Z"
+                        fill="#00FDEE"
+                      />
+                    </svg>
+                  </a>
+                </div>
               </div>
             </section>
-            
+            <div className="  flex flex-col gap-[85px]">
+              <p
+                className={`w-[500px]  py-4 pl-20 bg-gradient-to-l from-[#8F00FF] to-black     text-lg tracking-wide   md:leading-6  md:text-xl text-[#c7c7c7]  ${SpaceGrotesk.className}`}
+              >
+                I am open to remote and onsite full-&nbsp;time, part-time, and
+                contract frontend web development jobs.
+              </p>
+              <p
+                className={`text-right  text-[#000] dark:text-[#c7c7c7] ${SpaceGrotesk.className}`}
+              >
+                Designed and built by{" "}
+                <span className=" text-[#00FDEE] text-xl">Osita</span> with{" "}
+                <span className=" heading-txt text-xl">Love</span> &{" "}
+                <span className=" heading-txt text-xl">Coffee</span>.
+              </p>
+            </div>
           </div>
         </div>
         <form className=" w-[50%]  flex flex-col  gap-6 ">
