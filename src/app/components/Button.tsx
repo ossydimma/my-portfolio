a4bc67570b51eac0e15768b345eda1542d164ea2
Osita <chrisjerry070@gmail.com>
2024-01-22 13:45:20 +0400

    created btn component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -12,10 +12,10 @@ export const Button = ({href, text, div, styles} : buttonType) => {
         <a
             href={href}
             download
-            className= {`flex  gap-3 items-center shadow-2xl   py-4 px-5 sm:px-6  ${styles}`} 
+            className= {`flex gap-1.5  items-center shadow-2xl   py-4 px-5 sm:px-6 sm:gap-3 ${styles}`} 
             >
             {div}
-            <span className={` text-shadow text-white font-bold text-[10px] sm:text-[16px]`}>{text} </span>
+            <span className={` text-shadow text-white font-bold text-xs sm:text-[16px]`}>{text} </span>
         </a>
 
     </div>
